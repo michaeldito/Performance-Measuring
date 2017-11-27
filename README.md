8cec232366060e73cc9cf8797443ecca3d5c9c00
Michael Dito <mcdito13@gmail.com>
2017-11-26 21:42:46 -0800

    output and readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Performance-Measuring
-Scripts used for measuring the performance of a Google Cloud Application, using Apache Benchmark.
+Scripts used for measuring the performance of a Google Cloud Application, using Apache Benchmark. 
+
+We measured the performance of a simple REST API at various concurency levels in order to gauge how many REST servers were needed so that we could meet the expectations of our users.
+
+The bash script will run tests for all concurency levels, and output the results to files. These files are then loaded into the graphing script, and graphs are generated using Plotly.
--- graph.py
+++ graph.py
@@ -11,45 +11,22 @@ def readFileIntoArray(A, F):
 		i = i.rstrip('\n')
 		A.append(i)
 
-#t98 = open("2-data/2-98.txt", "r")
 tMean = open("2-data/2-mean.txt", "r")
-#two98 = []
 twoMean = []
-#readFileIntoArray(two98, t98)
 readFileIntoArray(twoMean, tMean)
 
-#th98 = open("3-data/3-98.txt", "r")
 thMean = open("3-data/3-mean.txt", "r")
-#three98 = []
 threeMean = []
-#readFileIntoArray(three98, th98)
 readFileIntoArray(threeMean, thMean)
 
-#f98 = open("4-data/4-98.txt", "r")
 fMean = open("4-data/4-mean.txt", "r")
-#four98 = []
 fourMean = []
-#readFileIntoArray(four98, f98)
 readFileIntoArray(fourMean, fMean)
 
-#fv98 = open("5-data/5-98.txt", "r")
 fvMean = open("5-data/5-mean.txt", "r")
-#five98 = []
 fiveMean = []
-#readFileIntoArray(five98, fv98)
 readFileIntoArray(fiveMean, fvMean)
 
-# Create and style traces
-'''
-traceTwo98 = go.Scatter(
-	x = cLevel,
-    	y = two98,
-   	name = '2-node 98%',
-    	line = dict(
-        	color = ('rgb(205, 12, 24)'),
-        	width = 4)
-)
-'''
 traceTwoMean = go.Scatter(
 	x = cLevel,
 	y = twoMean,
@@ -59,16 +36,7 @@ traceTwoMean = go.Scatter(
 		width = 4,
 	)
 )
-'''
-traceThree98 = go.Scatter(
-    	x = cLevel,
-   	y = three98,
-    	name = '3-node 98%',
-    	line = dict(
-        	color = ('rgb(34, 139, 34)'),
-        	width = 4)
-)
-'''
+
 traceThreeMean = go.Scatter(
 	x = cLevel,
 	y = threeMean,
@@ -78,16 +46,7 @@ traceThreeMean = go.Scatter(
 		width = 4,
 	)
 )
-'''
-traceFour98 = go.Scatter(
-    	x = cLevel,
-   	y = four98,
-    	name = '4-node 98%',
-    	line = dict(
-        	color = ('rgb(127, 0, 255)'),
-       		width = 4)
-)
-'''
+
 traceFourMean = go.Scatter(
 	x = cLevel,
 	y = fourMean,
@@ -97,16 +56,7 @@ traceFourMean = go.Scatter(
 		width = 4,
 	)
 )
-'''
-traceFive98 = go.Scatter(
-    	x = cLevel,
-   	y = five98,
-    	name = '5-node 98%',
-    	line = dict(
-        	color = ('rgb(255, 128, 0)'),
-       		width = 4)
-)
-'''
+
 traceFiveMean = go.Scatter(
 	x = cLevel,
 	y = fiveMean,
@@ -117,7 +67,6 @@ traceFiveMean = go.Scatter(
 	)
 )
 
-#data = [traceTwo98, traceTwoMean, traceThree98, traceThreeMean, traceFour98, traceFourMean, traceFive98, traceFiveMean]
 data = [traceTwoMean, traceThreeMean, traceFourMean, traceFiveMean]
 
 # Edit the layout
